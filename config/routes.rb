e0357386e326643d590535ad8e2f3c36c1529001
Dylan Ng <ngxinde@gmail.com>
2016-07-27 01:15:08 +0800

    added static pages controller, views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  root 'users#index'
+  root 'static_pages#home'
+
+  #restful routes (CRUD)
   resources :users
   resources :microposts
+
+  #static pages routes, no models
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
