0c04e6a3b5c9324cde539ac8f1df643eaf7426c5
Dylan Ng <ngxinde@gmail.com>
2016-07-27 17:03:56 +0800

    finished chapter 5, layout filling

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,7 +1,7 @@
 module StaticPagesHelper
 
   def heading_detail(heading)
-    base_heading = "Welcome to Toy"
+    base_heading = "Welcome to Blog Stop"
 
     if heading != ''
       base_heading = base_heading + ' | ' + heading
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
+  #static pages routes, not connected models
+  get '/home', to: 'static_pages#home'
+  get '/about', to: 'static_pages#about'
+  get '/help', to: 'static_pages#help'
+  #get '/contact', to: 'static_pages#contact'
+
+  # dynamic routes
+  get '/signup', to: 'users#new'
+
   #restful routes (CRUD)
-  resources :users
+  resources :users, except:[ :new ]
   resources :microposts
 
-  #static pages routes, no models
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
