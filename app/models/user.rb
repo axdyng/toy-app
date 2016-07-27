2e506e88d3f1c3ec9f6f6ec6dbd7c3f722649779
Dylan Ng <ngxinde@gmail.com>
2016-07-28 00:36:10 +0800

    added indices to email, included secure password with bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-  before_save { email.downcase! }
+  before_save { self.email.downcase! }
   # relationship
   has_many :microposts
 
   # validation
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   validates :name,
             presence: true,
@@ -14,4 +14,10 @@ class User < ApplicationRecord
             presence: true,
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
+
+  # hashing password
+  has_secure_password
+  validates :password,
+            presence: true,
+            length: { minimum: 12 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160726090333) do
+ActiveRecord::Schema.define(version: 20160727154457) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -22,8 +22,10 @@ ActiveRecord::Schema.define(version: 20160726090333) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
